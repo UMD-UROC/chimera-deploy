d6cb65bc1b637de78be631d34b6e233b1a6a925c
ctitus1 <ctitus@umd.edu>
2026-02-16 11:33:12 -0500

    ip and cam server changes

--- local/local_start_camera_server.sh
+++ local/local_start_camera_server.sh
@@ -64,22 +64,22 @@ PIPES=()
 common="protocols=udp latency=0 drop-on-late=true ! rtph265depay ! queue2 leaky=downstream max-size-buffers=1 ! h265parse ! video/x-h265,stream-format=byte-stream,alignment=au ! rtph265pay pt=96 config-interval=1 name=pay0 )"
 
 #TAGS+=("rgb1")
-#PIPES+=("( rtspsrc location=rtsp://10.200.91.51:8900/live $common")
+#PIPES+=("( rtspsrc location=rtsp://10.200.142.51:8900/live $common")
 
 #TAGS+=("rgb2")
-#PIPES+=("( rtspsrc location=rtsp://10.200.91.52:8900/live $common")
+#PIPES+=("( rtspsrc location=rtsp://10.200.142.52:8900/live $common")
 
 TAGS+=("rgb3")
-PIPES+=("( rtspsrc location=rtsp://10.200.91.53:8554/rgb $common")
+PIPES+=("( rtspsrc location=rtsp://10.200.142.53:8554/rgb $common")
 
 TAGS+=("thermal3")
-PIPES+=("( rtspsrc location=rtsp://10.200.91.53:8554/thermal $common")
+PIPES+=("( rtspsrc location=rtsp://10.200.142.53:8554/thermal $common")
 
 TAGS+=("rgb4")
-PIPES+=("( rtspsrc location=rtsp://10.200.91.54:8554/rgb $common")
+PIPES+=("( rtspsrc location=rtsp://10.200.142.54:8554/rgb $common")
 
 TAGS+=("thermal4")
-PIPES+=("( rtspsrc location=rtsp://10.200.91.54:8554/thermal $common")
+PIPES+=("( rtspsrc location=rtsp://10.200.142.54:8554/thermal $common")
 
 # Validate matching lengths
 if [ "${#TAGS[@]}" -ne "${#PIPES[@]}" ]; then
