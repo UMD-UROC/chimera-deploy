cf7e10a31aebc9c14c0fbdb05a25043c2d9d1ff5
ctitus1 <ctitus@umd.edu>
2026-04-16 10:53:41 -0400

    updated for thermal nv accel

--- remote/rtsp_config.py
+++ remote/rtsp_config.py
@@ -3,90 +3,62 @@
 HIRES_TAG = "hires"
 LOWRES_TAG = "lowres"
 
-HIRES_SOCKET = "/tmp/cam_hires.shm"
-LOWRES_SOCKET = "/tmp/cam_lowres.shm"
+HIRES_SOCKET = "/tmp/hires_nv.sock"
+LOWRES_SOCKET = "/tmp/lowres_nv.sock"
 
 HIRES_WIDTH = 640
-HIRES_HEIGHT = 480
-HIRES_BITRATE = 2000
+HIRES_HEIGHT = 512
+HIRES_BITRATE = 1000000
+HIRES_PEAK_BITRATE = 5000000
 
-LOWRES_WIDTH = 640
-LOWRES_HEIGHT = 480
-LOWRES_BITRATE = 200
+LOWRES_WIDTH = 320
+LOWRES_HEIGHT = 256
+LOWRES_BITRATE = 300000
+LOWRES_PEAK_BITRATE = 1000000
 
 SOCKETS = {
     HIRES_TAG: HIRES_SOCKET,
     LOWRES_TAG: LOWRES_SOCKET,
 }
 
-# any producers that need to run to set up srcs for factories go here
-# ie clone video0 to shared memory sinks
-# should be 1 to many (otherwise just make it a factory)
-# PRODUCERS = {
-#     "video0-fork": f"""
-#         v4l2src device=/dev/video0 io-mode=mmap do-timestamp=true !
-#         video/x-raw,format=YUY2,width={HIRES_WIDTH},height={HIRES_HEIGHT},framerate=30/1 !
-#         videoconvert !
-#         video/x-raw,format=NV12 !
-#         tee name=t
-
-#         t. ! queue leaky=downstream max-size-buffers=1 max-size-bytes=0 max-size-time=0 !
-#         shmsink socket-path={HIRES_SOCKET} wait-for-connection=false sync=false async=false shm-size=67108864
-
-#         t. ! queue leaky=downstream max-size-buffers=1 max-size-bytes=0 max-size-time=0 !
-#         videoscale !
-#         videoconvert !
-#         video/x-raw,format=NV12,width={LOWRES_WIDTH},height={LOWRES_HEIGHT} !
-#         shmsink socket-path={LOWRES_SOCKET} wait-for-connection=false sync=false async=false shm-size=16777216
-#         """,
-# }
 PRODUCERS = {
-    "video0-fork": f"""
-        nvarguscamerasrc sensor-id=0 wbmode=1 !
+    "video1-fork": f"""
+        v4l2src device=/dev/video1 io-mode=2 !
         queue max-size-buffers=1 leaky=downstream !
-        video/x-raw(memory:NVMM),width=1920,height=1080,framerate=30/1 !
-        nvvidconv flip-method=2 !
-        video/x-raw(memory:NVMM) !
+        video/x-raw,width={HIRES_WIDTH},height={HIRES_HEIGHT},format=I420 !
+        nvvidconv !
+        video/x-raw(memory:NVMM),format=NV12 !
         tee name=t
 
         t. ! queue leaky=downstream max-size-buffers=1 max-size-bytes=0 max-size-time=0 !
-        nvv4l2h265enc control-rate=0 bitrate={HIRES_BITRATE} peak-bitrate=5000000 iframeinterval=0 insert-sps-pps=true EnableTwopassCBR=false !
-        h265parse !
-        rtph265pay config-interval=1 pt=96 !
-        shmsink socket-path={HIRES_SOCKET} wait-for-connection=false sync=false async=false shm-size=67108864
+        nvunixfdsink socket-path={HIRES_SOCKET} sync=false
 
         t. ! queue leaky=downstream max-size-buffers=1 max-size-bytes=0 max-size-time=0 !
         nvvidconv !
         video/x-raw(memory:NVMM),width={LOWRES_WIDTH},height={LOWRES_HEIGHT},format=NV12 !
-        nvv4l2h265enc control-rate=0 bitrate={LOWRES_BITRATE} peak-bitrate=5000000 iframeinterval=0 insert-sps-pps=true EnableTwopassCBR=false !
-        h265parse !
-        rtph265pay config-interval=1 pt=96 !
-        shmsink socket-path={LOWRES_SOCKET} wait-for-connection=false sync=false async=false shm-size=16777216
+        nvunixfdsink socket-path={LOWRES_SOCKET} sync=false
         """,
 }
 
-# any rtsp factories go here
-# these are the layer before rtsp server
-# should be 1 to 1
 FACTORIES = {
     HIRES_TAG: f"""
         (
-        shmsrc socket-path={HIRES_SOCKET} is-live=true do-timestamp=true !
-        application/x-rtp,media=video,encoding-name=H265,payload=96,clock-rate=90000 !
+        nvunixfdsrc socket-path={HIRES_SOCKET} !
+        video/x-raw(memory:NVMM),format=NV12,width={HIRES_WIDTH},height={HIRES_HEIGHT} !
         queue leaky=downstream max-size-buffers=1 max-size-bytes=0 max-size-time=0 !
-        rtph265depay !
-        h265parse config-interval=-1 !
-        rtph265pay name=pay0 pt=96 config-interval=-1
+        nvv4l2h265enc maxperf-enable=1 control-rate=0 bitrate={HIRES_BITRATE} peak-bitrate={HIRES_PEAK_BITRATE} iframeinterval=30 insert-sps-pps=true EnableTwopassCBR=false !
+        h265parse !
+        rtph265pay name=pay0 pt=96 config-interval=1
         )
         """,
     LOWRES_TAG: f"""
         (
-        shmsrc socket-path={LOWRES_SOCKET} is-live=true do-timestamp=true !
-        application/x-rtp,media=video,encoding-name=H265,payload=96,clock-rate=90000 !
+        nvunixfdsrc socket-path={LOWRES_SOCKET} !
+        video/x-raw(memory:NVMM),format=NV12,width={LOWRES_WIDTH},height={LOWRES_HEIGHT} !
         queue leaky=downstream max-size-buffers=1 max-size-bytes=0 max-size-time=0 !
-        rtph265depay !
-        h265parse config-interval=-1 !
-        rtph265pay name=pay0 pt=96 config-interval=-1
+        nvv4l2h265enc maxperf-enable=1 control-rate=0 bitrate={LOWRES_BITRATE} peak-bitrate={LOWRES_PEAK_BITRATE} iframeinterval=30 insert-sps-pps=true EnableTwopassCBR=false !
+        h265parse !
+        rtph265pay name=pay0 pt=96 config-interval=1
         )
         """,
 }
