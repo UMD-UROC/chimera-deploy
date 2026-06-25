95d3b62d8cf9dd646eb9e80d208f2966d8d370b6
ctitus1 <ctitus@umd.edu>
2026-06-25 13:52:18 -0400

    separate socks for all

--- remote/rtsp_config.py
+++ remote/rtsp_config.py
@@ -6,16 +6,16 @@ RGB_FRAMERATE = "30/1"
 # RGB_WIDTH = 1920
 # RGB_HEIGHT = 1080
 # RGB_FRAMERATE = "60/1"
-RGB_BITRATE = 1000000
+RGB_BITRATE = 160000000
 
 # locked to 1080p for USPI, can increase with better laptop probably
-RGB_LOWRES_WIDTH = 1920
-RGB_LOWRES_HEIGHT = 1080
-RGB_LOWRES_BITRATE = 10000000
+RGB_LOWRES_WIDTH = 640
+RGB_LOWRES_HEIGHT = 360
+RGB_LOWRES_BITRATE = 1000000
 
 THERMAL_WIDTH = 640
 THERMAL_HEIGHT = 512
-THERMAL_BITRATE = 40000
+THERMAL_BITRATE = 40000000
 
 # THERMAL_LOWRES_WIDTH = 640
 # THERMAL_LOWRES_HEIGHT = 512
@@ -24,9 +24,11 @@ THERMAL_LOWRES_HEIGHT = THERMAL_HEIGHT
 THERMAL_LOWRES_BITRATE = 400000
 
 RGB = "rgb"
+RGB_DEEPSTREAM = "rgbds"
 RGB_LOWRES = "rgbl"
 RGB_RAW = "rgbraw"
 THERMAL = "thermal"
+THERMAL_DEEPSTREAM = "thermalds"
 THERMAL_LOWRES = "thermall"
 THERMAL_RAW = "thermalraw"
 
@@ -35,9 +37,11 @@ def SOCKET(tag):
 
 SOCKETS = {
     RGB: SOCKET(RGB),
+    RGB_DEEPSTREAM: SOCKET(RGB_DEEPSTREAM),
     RGB_LOWRES: SOCKET(RGB_LOWRES),
     RGB_RAW: SOCKET(RGB_RAW),
     THERMAL: SOCKET(THERMAL),
+    THERMAL_DEEPSTREAM: SOCKET(THERMAL_DEEPSTREAM),
     THERMAL_LOWRES: SOCKET(THERMAL_LOWRES),
     THERMAL_RAW: SOCKET(THERMAL_RAW),
 }
@@ -52,6 +56,9 @@ PRODUCERS = {
 
         t. ! queue leaky=downstream max-size-buffers=1 !
         nvunixfdsink socket-path={SOCKETS[RGB]} sync=false
+
+        t. ! queue leaky=downstream max-size-buffers=1 !
+        nvunixfdsink socket-path={SOCKETS[RGB_DEEPSTREAM]} sync=false
 
         t. ! queue leaky=downstream max-size-buffers=1 !
         nvvidconv interpolation-method=1 !
@@ -75,6 +82,9 @@ PRODUCERS = {
         nvunixfdsink socket-path={SOCKETS[THERMAL]} sync=false
 
         t. ! queue leaky=downstream max-size-buffers=1 !
+        nvunixfdsink socket-path={SOCKETS[THERMAL_DEEPSTREAM]} sync=false
+
+        t. ! queue leaky=downstream max-size-buffers=1 !
         nvvidconv interpolation-method=1 !
         video/x-raw(memory:NVMM),width={THERMAL_LOWRES_WIDTH},height={THERMAL_LOWRES_HEIGHT},format=NV12 !
         tee name=tl
@@ -93,7 +103,7 @@ FACTORIES = {
         nvunixfdsrc socket-path={SOCKETS[RGB]} num-extra-surfaces=4 do-timestamp=true !
         video/x-raw(memory:NVMM),format=NV12,width={RGB_WIDTH},height={RGB_HEIGHT} !
         queue leaky=downstream max-size-buffers=1 !
-        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={RGB_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false zerolatency=true !
+        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={RGB_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false !
         h265parse !
         rtph265pay name=pay0 pt=96 config-interval=1
         )
@@ -103,7 +113,7 @@ FACTORIES = {
         nvunixfdsrc socket-path={SOCKETS[RGB_LOWRES]} num-extra-surfaces=4 do-timestamp=true !
         video/x-raw(memory:NVMM),format=NV12,width={RGB_LOWRES_WIDTH},height={RGB_LOWRES_HEIGHT} !
         queue leaky=downstream max-size-buffers=1 !
-        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={RGB_LOWRES_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false zerolatency=true !
+        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={RGB_LOWRES_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false !
         h265parse !
         rtph265pay name=pay0 pt=96 config-interval=1
         )
@@ -113,7 +123,7 @@ FACTORIES = {
         nvunixfdsrc socket-path={SOCKETS[THERMAL]} num-extra-surfaces=4 do-timestamp=true !
         video/x-raw(memory:NVMM),format=NV12,width={THERMAL_WIDTH},height={THERMAL_HEIGHT} !
         queue leaky=downstream max-size-buffers=1 !
-        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={THERMAL_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false zerolatency=true !
+        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={THERMAL_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false !
         h265parse !
         rtph265pay name=pay0 pt=96 config-interval=1
         )
@@ -123,7 +133,7 @@ FACTORIES = {
         nvunixfdsrc socket-path={SOCKETS[THERMAL_LOWRES]} num-extra-surfaces=4 do-timestamp=true !
         video/x-raw(memory:NVMM),format=NV12,width={THERMAL_LOWRES_WIDTH},height={THERMAL_LOWRES_HEIGHT} !
         queue leaky=downstream max-size-buffers=1 !
-        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={THERMAL_LOWRES_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false zerolatency=true !
+        nvv4l2h265enc maxperf-enable=1 control-rate=1 bitrate={THERMAL_LOWRES_BITRATE} iframeinterval=30 idrinterval=30 insert-sps-pps=true insert-vui=true EnableTwopassCBR=false !
         h265parse !
         rtph265pay name=pay0 pt=96 config-interval=1
         )
