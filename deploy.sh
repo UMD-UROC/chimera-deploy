93cc78e73da6c9902cb542387169afd72c506a45
ctitus1 <ctitus@umd.edu>
2026-06-28 23:00:26 -0400

    added more detail

--- deploy.sh
+++ deploy.sh
@@ -59,13 +59,13 @@ sudo sed -i 's/^DEFAULT .*/DEFAULT Custom/' /boot/extlinux/extlinux.conf
 
 # rtsp server
 sudo apt install -y gir1.2-gst-rtsp-server-1.0 python3-gi
-cp remote/multi_rtsp_server.py ~
 
 # rtsp server as service
 sudo cp remote/rcam.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start rcam.service
 sudo systemctl enable rcam.service
+
 
 # echomav deploy
 cd submodules/echopilot_deploy/
@@ -78,55 +78,124 @@ envsubst < ./remote/main.conf.template | sudo tee /etc/mavlink-router/main.conf 
 sudo systemctl enable mavlink-router.service
 sudo systemctl restart mavlink-router.service
 
-# use nmcli to configure the network appropriately
-if ! nmcli -t -f NAME connection show | grep -Fxq "RoboScout-silvus"; then
-    sudo nmcli connection add type ethernet \
-      ifname eno1 con-name RoboScout-silvus \
-      ipv4.method manual ipv4.addresses 10.200.142.5${UAS_NUM}/24 \
-      connection.autoconnect yes
-else
-    echo "Existing \"RoboScout-silvus\" connection detected — skipping creation."
-fi
-
-  
-# use nmtui to add any other connections and modify to preference
-
-# ssh
-sudo ssh-keygen -A
-sudo systemctl restart ssh
-
-sudo apt autoremove -y
-
-# get docker containers for ml
-# install the container tools
-git clone https://github.com/dusty-nv/jetson-containers
-bash jetson-containers/install.sh
-
-# install yolo stuff for cuda
-pip3 uninstall -y torch torchvision torchaudio
-pip3 install https://pypi.jetson-ai-lab.io/jp6/cu126/+f/02f/de421eabbf626/torch-2.9.1-cp310-cp310-linux_aarch64.whl#sha256=02fde421eabbf62633092de30405ea4d917323c55bea22bfd10dfeb1f1023506 # torch 2.9.1 cuda
-pip3 install https://pypi.jetson-ai-lab.io/jp6/cu126/+f/d12/bede7113e6b00/torchaudio-2.9.1-cp310-cp310-linux_aarch64.whl#sha256=d12bede7113e6b00f7c5ed53a28f7fa44a624780c8097a6a2352f32548d77ffb # torch audio 2.9.1 cuda
-pip3 install https://pypi.jetson-ai-lab.io/jp6/cu126/+f/d5b/caaf709f11750/torchvision-0.24.1-cp310-cp310-linux_aarch64.whl#sha256=d5bcaaf709f11750b5bb0f6ec30f37605da2f3d5cb3cd2b0fe5fac2850e08642 # torch vision 2.9.1 cuda
-
-pip3 uninstall -y ultralytics
-pip3 install ultralytics
-
-cd
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/arm64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cusparselt-cuda-12 cudss libcudnn9-cuda-12 # may only need cudss
-
-# mavros
-sudo apt install ros-humble-mavros
-wget https://raw.githubusercontent.com/mavlink/mavros/ros2/mavros/scripts/install_geographiclib_datasets.sh
-chmod +x install_geographiclib_datasets.sh
-sudo ./install_geographiclib_datasets.sh
-
-echo "Done, don't forget to set network settings if haven't already. Power cycle to and confirm ssh connects from host to complete!"
-
 # chrony
 sudo apt install chrony -y
 grep -qxF "server 10.200.142.60 iburst" /etc/chrony/chrony.conf || echo "server 10.200.142.60 iburst" | sudo tee -a /etc/chrony/chrony.conf # add 10.200.142.60 as chrony server
 sudo systemctl restart chrony
 date # verify
+
+# quit before wip stuff
+exit()
+
+### follow docs for ros install
+
+# ros message packages
+sudo apt install -y \
+  ros-humble-builtin-interfaces \
+  ros-humble-domain-bridge \
+  ros-humble-geographic-msgs \
+  ros-humble-geometry-msgs \
+  ros-humble-mavros-msgs \
+  ros-humble-nav-msgs \
+  ros-humble-rcl-interfaces \
+  ros-humble-sensor-msgs \
+  ros-humble-std-msgs \
+  ros-humble-vision-msgs \
+  ros-humble-visualization-msgs
+  
+
+cdr
+sudo rosdep init
+rosdep install --from-paths src --ignore-src -r -y
+  
+  
+  # confirmed
+sudo apt install -y \
+  ros-humble-vision-msgs \
+  ros-humble-domain-bridge \
+  ros-humble-cv-bridge
+    
+pip install pymap3d folium
+
+# torch
+pip install --no-deps ultralytics
+pip uninstall -y torch torchvision torchaudio ultralytics && \
+pip install --no-cache-dir https://pypi.jetson-ai-lab.io/jp6/cu126/+f/02f/de421eabbf626/torch-2.9.1-cp310-cp310-linux_aarch64.whl#sha256=02fde421eabbf62633092de30405ea4d917323c55bea22bfd10dfeb1f1023506 # torch 2.9.1 cuda
+pip install --no-cache-dir https://pypi.jetson-ai-lab.io/jp6/cu126/+f/d12/bede7113e6b00/torchaudio-2.9.1-cp310-cp310-linux_aarch64.whl#sha256=d12bede7113e6b00f7c5ed53a28f7fa44a624780c8097a6a2352f32548d77ffb # torch audio 2.9.1 cuda
+pip install --no-cache-dir https://pypi.jetson-ai-lab.io/jp6/cu126/+f/d5b/caaf709f11750/torchvision-0.24.1-cp310-cp310-linux_aarch64.whl#sha256=d5bcaaf709f11750b5bb0f6ec30f37605da2f3d5cb3cd2b0fe5fac2850e08642 # torch vision 2.9.1 cuda
+pip install --user --force-reinstall 'numpy<2'
+pip install --no-deps ultralytics
+pip install onnx
+pip install ftfy regex tqdm
+pip install git+https://github.com/openai/CLIP.git
+pip install onnxscript
+
+# cuda ss
+cd ~
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/arm64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt install -y cudss
+
+# pyds
+# may not need these two lines
+sudo /opt/nvidia/deepstream/deepstream/install.sh
+sudo ldconfig
+
+cd /tmp
+wget -O pyds-1.2.0-cp310-cp310-linux_aarch64.whl \
+  https://github.com/NVIDIA-AI-IOT/deepstream_python_apps/releases/download/v1.2.0/pyds-1.2.0-cp310-cp310-linux_aarch64.whl
+python3 -m pip install --user --force-reinstall \
+  ./pyds-1.2.0-cp310-cp310-linux_aarch64.whl
+
+# mavros (after ros install)
+sudo apt install -y ros-humble-mavros ros-humble-mavros-extras
+wget https://raw.githubusercontent.com/mavlink/mavros/ros2/mavros/scripts/install_geographiclib_datasets.sh
+chmod +x install_geographiclib_datasets.sh
+sudo ./install_geographiclib_datasets.sh
+
+#ros domain id
+echo 'export ROS_DOMAIN_ID=64' >> ~/.bashrc
+source ~/.bashrc
+
+# get docker containers for ml
+# install the container tools
+#git clone https://github.com/dusty-nv/jetson-containers
+#bash jetson-containers/install.sh
+
+# install yolo stuff for cuda
+#pip3 uninstall -y torch torchvision torchaudio
+#pip3 install https://pypi.jetson-ai-lab.io/jp6/cu126/+f/02f/de421eabbf626/torch-2.9.1-cp310-cp310-linux_aarch64.whl#sha256=02fde421eabbf62633092de30405ea4d917323c55bea22bfd10dfeb1f1023506 # torch 2.9.1 cuda
+#pip3 install https://pypi.jetson-ai-lab.io/jp6/cu126/+f/d12/bede7113e6b00/torchaudio-2.9.1-cp310-cp310-linux_aarch64.whl#sha256=d12bede7113e6b00f7c5ed53a28f7fa44a624780c8097a6a2352f32548d77ffb # torch audio 2.9.1 cuda
+#pip3 install https://pypi.jetson-ai-lab.io/jp6/cu126/+f/d5b/caaf709f11750/torchvision-0.24.1-cp310-cp310-linux_aarch64.whl#sha256=d5bcaaf709f11750b5bb0f6ec30f37605da2f3d5cb3cd2b0fe5fac2850e08642 # torch vision 2.9.1 cuda
+
+#pip3 uninstall -y ultralytics
+#pip3 install ultralytics
+
+#cd
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/arm64/cuda-keyring_1.1-1_all.deb
+#sudo dpkg -i cuda-keyring_1.1-1_all.deb
+#sudo apt-get update
+#sudo apt-get -y install cusparselt-cuda-12 cudss libcudnn9-cuda-12 # may only need cudss
+
+# use nmcli to configure the network appropriately
+if ! nmcli -t -f NAME connection show | grep -Fxq "RoboScout-silvus"; then
+    sudo nmcli connection add type ethernet \
+      ifname eno1 con-name RoboScout-silvus \
+      ipv4.method manual ipv4.addresses 10.200.142.6${UAS_NUM}/24 \
+      connection.autoconnect yes
+else
+    echo "Existing \"RoboScout-silvus\" connection detected — skipping creation."
+fi
+
+sudo apt autoremove -y
+
+echo "Done, don't forget to set network settings if haven't already. Power cycle to and confirm ssh connects from host to complete!"
+
+# install jtop
+#sudo pip install jetson-stats
+
+## REMEMBER TO SET TO 25W
+
+
+
